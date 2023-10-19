4d8580ae5dfbc7d1be34341265b8f46691b01551
Pranav Shikarpur <pranav.shikarpur@pangea.cloud>
2023-10-19 17:09:47 -0400

    fixed ip detect issues

--- src/pages/api/geolocate.ts
+++ src/pages/api/geolocate.ts
@@ -2,14 +2,15 @@
 import { withAPIAuthentication } from '@/lib/pangeaMiddleware';
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { PangeaConfig, IPIntelService, PangeaErrors } from "pangea-node-sdk";
+import requestIp from 'request-ip';
 
 function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-    const clientIP = req.connection.remoteAddress
+    const detectedIP = requestIp.getClientIp(req);
 
-    res.status(200).json({"message": clientIP})
+    res.status(200).json({"message": detectedIP})
 }
 
 export default withAPIAuthentication(handler)
